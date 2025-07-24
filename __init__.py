74b5c3055f8e80aecdd4010ee102789d9bca809b
CallMe1101 <z15194913998@outlook.com>
2025-07-24 17:38:18 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,13 @@
+# coding: utf-8
+"""
+OmniAvatar ComfyUI 自定义节点包
+注册 OmniAvatar All-In-One 节点
+"""
+
+# 导入节点类
 from .node_omniavatar_allinone import OmniAvatarAllInOneNode
 
-NODE_CLASS_MAPPINGS = {"OmniAvatarAllInOneNode": OmniAvatarAllInOneNode}
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "OmniAvatarAllInOneNode": "OmniAvatar All-in-One (14B)"
+# 节点映射：键为用户在 UI 中看到的名称，值为对应的类
+NODE_CLASS_MAPPINGS = {
+    "OmniAvatar All-in-One (14B)": OmniAvatarAllInOneNode,
 }
